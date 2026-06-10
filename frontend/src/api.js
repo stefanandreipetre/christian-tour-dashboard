056119d8eddc5cf019f21b917683754f39bea44b
stefanandreipetre <stefan.andrei.petre@gmail.com>
2026-06-10 09:49:00 +0300

    Fix 3 bugs: Overview LY chart, B2B Anterior column, YTD table selector

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,8 +10,8 @@ export const getStatus         = ()                    => api.get('/api/status')
 
 // B2C
 export const getB2CSummary     = (year, month)         => api.get('/api/b2c/summary',  { params: { year, month } }).then(r => r.data)
-export const getB2CMonthly     = (year)                => api.get('/api/b2c/monthly',  { params: { year, ...(maxMonth ? { max_month: maxMonth } : {}) } }).then(r => r.data)
-export const getB2CBranches    = (year, maxMonth) => api.get('/api/b2c/branches', { params: { year, ...(maxMonth ? { max_month: maxMonth } : {}) } }).then(r => r.data)
+export const getB2CMonthly     = (year)                => api.get('/api/b2c/monthly',  { params: { year } }).then(r => r.data)
+export const getB2CBranches    = (year, maxMonth)      => api.get('/api/b2c/branches', { params: { year, ...(maxMonth ? { max_month: maxMonth } : {}) } }).then(r => r.data)
 
 // B2B
 export const getB2BSummary     = (year, month)         => api.get('/api/b2b/summary',  { params: { year, month } }).then(r => r.data)
@@ -26,7 +26,7 @@ export const getB2CDaily       = empty
 export const getB2CWeekly      = empty
 export const getB2BYearly      = empty
 export const getB2BRecent      = (n)  => getB2BMonthly()
-export const getB2BAgencies    = (year, maxMonth) => api.get('/api/b2b/partners', { params: { year } }).then(r => r.data)
+export const getB2BAgencies    = (year, maxMonth) => api.get('/api/b2b/partners', { params: { year, ...(maxMonth ? { max_month: maxMonth } : {}) } }).then(r => r.data)
 export const getB2BBranches    = empty
 export const getB2BvsTarget    = empty
 export const getOutlookMonthly = empty
